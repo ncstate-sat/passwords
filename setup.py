c67530bb415afb500b284853212db5d8ab3624f5
Zane Helton <zane99@me.com>
2022-07-29 18:10:32 -0400

    Add setup.py for pip to build & install

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+setup(
+    author="SAT",
+    classifiers=[
+        'License :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description="A package to retrieve passwords from the Passwordstate API",
+    license="MIT license",
+    include_package_data=True,
+    name='Passwords',
+    version='0.1.0',
+    zip_safe=False,
+)
